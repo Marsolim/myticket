2067c97b03237a5b583b3298f78adec4d1ff169d
Marsolim <darknessfallenstar@gmail.com>
2023-08-04 22:41:36 +0700

    polishing attachment section

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -15,7 +15,7 @@ class AppAsset extends AssetBundle
         'css/site.css',
     ];
     public $js = [
-        'assets/local-js/dropdown-button-ajax.js'
+        'assets/local-js/dropdown-button-ajax.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
--- frontend/widgets/views/_stepprogress.php
+++ frontend/widgets/views/_stepprogress.php
@@ -3,6 +3,7 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use yii\helpers\ArrayHelper;
+use yii\helpers\Url;
 use frontend\helpers\RoleHelper;
 use frontend\helpers\TStatusHelper;
 use common\models\User;
@@ -16,39 +17,44 @@ $steps = $model->getModels();
 ArrayHelper::multisort($steps, ['action_date'], [SORT_DESC]);
 $last = end($steps);
 ?>
-<div id="<?= $id ?>">
+<?= Html::beginTag('div', ['id' => $id]) ?>
 <?php foreach($steps as $index => $step) {
   $sto = TStatusHelper::getLabel($step); 
   $date = date('d F Y H:i.s', $step->action_date);
   $documents = Document::findAll(['action_id' => $step->id]);
 ?>
 <div class="d-flex p-3 border-bottom">
-  <?= Html::img('uploads/profiles/thumb/'.$step->engineer->profile, ['class' => 'rounded-circle', 'loading' => 'lazy', 'alt'=>'profile', 'width' => '50','height'=>'50']) ?></span>
+  <?= Html::img('uploads/profiles/thumb/'.$step->engineer->profile, ['class' => 'rounded-circle', 'loading' => 'lazy', 'alt'=>'profile', 'style' => 'width:50px;height:50px']) ?></span>
   <div class="d-flex w-100 ps-3">
     <div>
-      <?= Html::a("
-      <h6 class='text-body'>
-      {$step->engineer->full_name}
-        <span class='small text-muted font-weight-normal'>{$step->engineer->email}</span>
-        <span class='small text-muted font-weight-normal'> • </span>
-        <span class='small text-muted font-weight-normal'>{$date}</span>
-      </h6>
-      ", ['user/view/', 'id' => $step->engineer_id]) ?>
-      <h5><?= $step->action ?></h5>
-      <p style="line-height: 1.2;">
-        <?= $step->summary ?>
-      </p>
-      <ul class="list-unstyled d-flex justify-content-between mb-0 pe-xl-5">
-        <?php if (isset($documents)) { ?> 
-        <?php foreach($documents as $doc) { ?> 
-        <?= Html::tag('li', Html::a($doc->fileIcon.' '.$doc->uploadname, ['document/download/', 'id' => $doc->id], ['class' => 'btn btn-link text-decoration-none'])) ?>
-        <?php } ?>
-        <?php } ?>
-      </ul>
+      <?= Html::beginTag('a', ['href' => Url::toRoute(['user/view/', 'id' => $step->engineer_id])]) ?>
+      <?= Html::beginTag('h6', ['class' => 'text-body']) ?>
+      <?= $step->engineer->full_name ?>
+      <?= Html::tag('span', $step->engineer->email, ['class' => 'small text-muted font-weight-normal']) ?>
+      <?= Html::tag('span', ' • ', ['class' => 'small text-muted font-weight-normal']) ?>
+      <?= Html::tag('span', $date, ['class' => 'small text-muted font-weight-normal']) ?> 
+      <?= Html::endTag('h6') ?> 
+      <?= Html::endTag('a') ?>
+      <?= Html::tag('h6', $step->action) ?>
+      <?= Html::tag('p', $step->summary, ['style'=>'line-height: 1.2;']) ?>
+      <?php if (isset($documents)) {
+        $collid = Yii::$app->security->generateRandomString();
+      ?> 
+        <?= Html::button('Attachments', ['class'=>'btn btn-link text-decoration-none', 'data-bs-toggle' =>'collapse', 'data-bs-target' => '#'.$collid]) ?>
+        <?= Html::beginTag('div', ['id'=>$collid, 'class'=>'collapse']) ?>
+        a
+        <?= Html::ul($documents, ['class' => 'list-unstyled d-flex justify-content-between mb-0 pe-xl-5',
+              'item'=> function($item, $index){
+                  return Html::tag('li', 
+                  Html::a($doc->fileIcon.' '.$doc->uploadname, ['document/download/', 'id' => $doc->id], 
+                  ['class' => 'btn btn-link text-decoration-none']));
+              }]) ?> 
+        <?= Html::endTag('div') ?>
+      <?php } ?>
       <ul class="list-unstyled d-flex justify-content-between mb-0 pe-xl-5">
         <li><?= $sto ?></li>
       </ul>
     </div>
   </div>
-</div>
+<?= Html::endTag('div') ?>
 <?php } ?>
