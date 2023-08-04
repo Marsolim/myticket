8a27c4cb99673b4a2c0db9dcd8e3841c8e14b8ac
Marsolim <137026909+Marsolim@users.noreply.github.com>
2023-08-04 17:00:28 +0700

    add thread view fpr progress

--- frontend/helpers/TStatusHelper.php
+++ frontend/helpers/TStatusHelper.php
@@ -21,46 +21,52 @@ class TStatusHelper
             'icon' => '<i class="fa-solid fa-door-open fa-fw text-primary"></i>', 
             'label' => 'Open',
             'group' => 'Open',
+            'tooltip' => 'Buka tiket servis No. %number$s.',
             'role' => [User::ROLE_SYS_ADMINISTRATOR, User::ROLE_ADMINISTRATOR, User::ROLE_STORE_MANAGER],
             'url' => ['report', 'id' => null, 'cmd' => self::OPEN],
-            'next_status' => [self::CLOSED_NA, self::CLOSED_DOUBLE, self::PROGRESSING, self::RESOLVED, self::RESOLVED_IT, self::SUSPENDED]
+            'next_status' => [self::SUSPENDED, self::PROGRESSING, self::RESOLVED, self::RESOLVED_IT, self::CLOSED_NA, self::CLOSED_DOUBLE]
         ],
         self::SUSPENDED => [
             'icon' => '<i class="fa-solid fa-lock fa-fw text-danger"></i>',
             'label' => 'Suspended',
             'group' => 'Progress',
+            'tooltip' => 'Suspend tiket servis No. %number$s.',
             'role' => [User::ROLE_SYS_ADMINISTRATOR, User::ROLE_ADMINISTRATOR],
             'url' => ['report', 'id' => null, 'cmd' => self::SUSPENDED],
-            'next_status' => [self::CLOSED_NA, self::OPEN]
+            'next_status' => [self::OPEN, self::CLOSED_NA]
         ],
         self::PROGRESSING => [
             'icon' => '<i class="fa-solid fa-list fa-fw"></i>', 
             'label' => 'Progress',
             'group' => 'Progress',
+            'tooltip' => 'Buat laporan progress tiket servis No. %number$s.',
             'role' => [User::ROLE_SYS_ADMINISTRATOR, User::ROLE_ADMINISTRATOR, User::ROLE_ENGINEER],
             'url' => ['report', 'id' => null, 'cmd' => self::PROGRESSING],
-            'next_status' => [self::PROGRESSING, self::RESOLVED, self::RESOLVED_IT, self::SUSPENDED]
+            'next_status' => [self::SUSPENDED, self::PROGRESSING, self::RESOLVED, self::RESOLVED_IT, self::CLOSED_NA]
         ],
         self::RESOLVED => [
-            'icon' => '<i class="fa-solid fa-list-check fa-fw"></i>', 
+            'icon' => '<i class="fa-solid fa-list-check fa-fw text-success"></i>', 
             'label' => 'Resolved (no Issue)',
             'group' => 'Progress',
+            'tooltip' => 'Buat laporan resolusi tiket servis No. %number$s.',
             'role' => [User::ROLE_SYS_ADMINISTRATOR, User::ROLE_ADMINISTRATOR, User::ROLE_ENGINEER],
             'url' => ['report', 'id' => null, 'cmd' => self::RESOLVED],
-            'next_status' => [self::CLOSED_RESOLVED, self::SUSPENDED]
+            'next_status' => [self::SUSPENDED, self::CLOSED_RESOLVED]
         ],
         self::RESOLVED_IT => [
-            'icon' => '<i class="fa-solid fa-list-check fa-fw "></i>', 
+            'icon' => '<i class="fa-solid fa-laptop-code fa-fw text-success"></i>', 
             'label' => 'Resolved (waiting for IT)',
             'group' => 'Progress',
+            'tooltip' => 'Buat laporan resolusi tiket servis No. %number$s.',
             'role' => [User::ROLE_SYS_ADMINISTRATOR, User::ROLE_ADMINISTRATOR, User::ROLE_ENGINEER],
             'url' => ['report', 'id' => null, 'cmd' => self::RESOLVED_IT],
-            'next_status' => [self::CLOSED_RESOLVED, self::SUSPENDED]
+            'next_status' => [self::SUSPENDED, self::CLOSED_RESOLVED]
         ],
         self::CLOSED_NA => [
             'icon' => '<i class="fa-solid fa-circle-xmark fa-fw text-danger"></i>', 
             'label' => 'Closed (no Action)',
             'group' => 'Closed',
+            'tooltip' => 'Tutup tiket servis No. %number$s karena tidak ada tindakan.',
             'role' => [User::ROLE_SYS_ADMINISTRATOR, User::ROLE_ADMINISTRATOR, User::ROLE_STORE_MANAGER],
             'url' => ['report', 'id' => null, 'cmd' => self::CLOSED_NA]
         ],
@@ -68,13 +74,15 @@ class TStatusHelper
             'icon' => '<i class="fa-solid fa-circle-xmark fa-fw text-warning"></i>', 
             'label' => 'Closed (duplicate Ticket)',
             'group' => 'Closed',
+            'tooltip' => 'Tutup tiket servis No. %number$s karena double input.',
             'role' => [User::ROLE_SYS_ADMINISTRATOR, User::ROLE_ADMINISTRATOR, User::ROLE_STORE_MANAGER],
             'url' => ['report', 'id' => null, 'cmd' => self::CLOSED_DOUBLE]
         ],
         self::CLOSED_RESOLVED => [
-            'icon' => '<i class="fa-solid fa-circle-check fa-fw text-safe"></i>', 
+            'icon' => '<i class="fa-solid fa-circle-check fa-fw text-success"></i>', 
             'label' => 'Closed (resolution accepted)',
             'group' => 'Closed',
+            'tooltip' => 'Tutup tiket servis No. %number$s.',
             'role' => [User::ROLE_SYS_ADMINISTRATOR, User::ROLE_ADMINISTRATOR, User::ROLE_STORE_MANAGER],
             'url' => ['report', 'id' => null, 'cmd' => self::CLOSED_RESOLVED]
         ],
@@ -82,8 +90,8 @@ class TStatusHelper
 
     private static $groups = [
         'Open' => [self::OPEN],
-        'Suspend' => [self::SUSPENDED],
         'Progress' => [self::PROGRESSING, self::RESOLVED, self::RESOLVED_IT],
+        'Suspend' => [self::SUSPENDED],
         'Closed' => [self::CLOSED_NA, self::CLOSED_DOUBLE, self::CLOSED_RESOLVED],
     ];
 
@@ -91,6 +99,7 @@ class TStatusHelper
     {
         $role = User::getUserRoleName();
         $ts = TicketStatus::findOne(['id' => $ticket->last_status_id]);
+        $open = self::$statuses[self::OPEN];
         if (isset($ts))
         {
             $status = self::$statuses[$ts->code];
@@ -107,14 +116,20 @@ class TStatusHelper
                         [
                             'class' => 'text-decoration-none',
                             'arial-label' => $st['label'],
-                            'title' => "Buka service no. {$ticket->number}."
+                            'title' => StringHelper::sprintfn($st['tooltip'], ['number' => $ticket->number])
                         ]);
                     }
                 }
                 return implode('  ', $nextsts);
             }
         }
-        return null;
+        else return Html::a($open['icon'], $open['url'], 
+        [
+            'class' => 'text-decoration-none',
+            'arial-label' => $open['label'],
+            'title' => StringHelper::sprintfn($open['tooltip'], ['number' => $ticket->number])
+        ]);
+        return '';
     }
 
     public static function createStep($taction)
@@ -127,11 +142,11 @@ class TStatusHelper
         ];
     }
 
-    public static function getLabel($role = 'default', $showtext = true)
+    public static function getLabel($taction, $showtext = true)
     {
-        $label = self::$labels[$role];
-        if ($showtext) return implode(' ', $label);
-        return $label['icon'];
+        $status = self::$statuses[$taction->status->code];
+        if ($showtext) return implode(' ', [$status['icon'], $status['label']]);
+        return $status['icon'];
     }
 
     public static function getRoleLabelOptions($user = null)
--- frontend/widgets/views/_stepprogress.php
+++ frontend/widgets/views/_stepprogress.php
@@ -6,57 +6,49 @@ use yii\helpers\ArrayHelper;
 use frontend\helpers\RoleHelper;
 use frontend\helpers\TStatusHelper;
 use common\models\User;
+use common\models\Document;
 
 /** @var yii\web\View $this */
 /** @var app\models\UserProfile $model */
 /** @var yii\widgets\ActiveForm $form */
 
 $steps = $model->getModels();
+ArrayHelper::multisort($steps, ['action_date'], [SORT_DESC]);
 $last = end($steps);
 ?>
-
+<div id="<?= $id ?>">
 <?php foreach($steps as $index => $step) {
-  $sto = TStatusHelper::createStep($step); 
-    $class = ["step"];
-    $i = $index + 1;
-    if ($index == (count($steps) - 1)) $class[] = 'step-active';
-    else $class[] = 'step-done';
-    if ($i < count($steps)) $i = '<i class="fa fa-check"></i>';
+  $sto = TStatusHelper::getLabel($step); 
+  $date = date('d F Y H:i.s', $step->action_date);
+  $documents = Document::findAll(['action_id' => $step->id]);
 ?>
-<div class="<?= implode(' ', $class) ?>">
-  <div>
-    <div class="circle"><?= $i ?></div>
-  </div>
-  <div>
-    <div class="title"><?= $sto['title'] ?></div>
-    <div class="caption"><?= $sto['caption']['content'] ?></div>
+<div class="d-flex p-3 border-bottom">
+  <?= Html::img('uploads/profiles/thumb/'.$step->engineer->profile, ['class' => 'rounded-circle', 'loading' => 'lazy', 'alt'=>'profile', 'width' => '50','height'=>'50']) ?></span>
+  <div class="d-flex w-100 ps-3">
+    <div>
+      <?= Html::a("
+      <h6 class='text-body'>
+      {$step->engineer->full_name}
+        <span class='small text-muted font-weight-normal'>{$step->engineer->email}</span>
+        <span class='small text-muted font-weight-normal'> • </span>
+        <span class='small text-muted font-weight-normal'>{$date}</span>
+      </h6>
+      ", ['user/view/', 'id' => $step->engineer_id]) ?>
+      <h5><?= $step->action ?></h5>
+      <p style="line-height: 1.2;">
+        <?= $step->summary ?>
+      </p>
+      <ul class="list-unstyled d-flex justify-content-between mb-0 pe-xl-5">
+        <?php if (isset($documents)) { ?> 
+        <?php foreach($documents as $doc) { ?> 
+        <?= Html::tag('li', Html::a($doc->fileIcon.' '.$doc->uploadname, ['document/download/', 'id' => $doc->id], ['class' => 'btn btn-link text-decoration-none'])) ?>
+        <?php } ?>
+        <?php } ?>
+      </ul>
+      <ul class="list-unstyled d-flex justify-content-between mb-0 pe-xl-5">
+        <li><?= $sto ?></li>
+      </ul>
+    </div>
   </div>
 </div>
 <?php } ?>
-
-<div class="step">
-  <div>
-    <div class="circle"><?= count($steps) + 1 ?></div>
-  </div>
-  <div>
-    <div class="title">Progress</div>
-    <div class="caption">This is description of second step.</div>
-  </div>
-</div>
-<div class="step">
-  <div>
-    <div class="circle">3</div>
-  </div>
-  <div>
-    <div class="title">Resolved</div>
-    <div class="caption">Some text about Third step. </div>
-  </div>
-</div>              
-<div class="step">
-  <div>
-    <div class="circle">4</div>
-  </div>
-  <div>
-    <div class="title">Closed</div>
-  </div>
-</div>
