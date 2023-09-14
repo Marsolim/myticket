d2e85fbbdc817ada1a1fb1692395f238127b6f72
Marsolim <137026909+Marsolim@users.noreply.github.com>
2023-09-14 16:50:22 +0700

    Ticket view updated

--- frontend/views/ticket/_ticket.php
+++ frontend/views/ticket/_ticket.php
@@ -161,3 +161,78 @@ $statuscolors = [
   </div>
 </div>
 </div>
+
+<div class="card position-relative text-primary mt-3 mb-2">
+    	<div class="card-header">
+        	<div class="d-flex justify-content-between p-2">
+            	<div class="d-flex flex-row align-items-center">
+                	<div class="d-flex flex-column ml-2">
+                        <i class="text-warning fa fa-3x fa-ticket"></i>
+                    </div>
+                    <div class="d-flex flex-column ms-2 ml-2">
+                        <div class="h6 position-relative">
+                        <span class="h6 position-relative"><span class="ticket ticket-number">TS0001</span><span class="ticket ticket-title">Pindah titik kamera 6</span>
+                        <span class="small badge rounded-pill bg-primary text-light">
+                                <a class="text-decoration-none text-light" title="Status ticket" href="#" data-method="POST">Belum dikunjungi</a>
+                                <span class="visually-hidden">unread messages</span>
+                            </span>
+                        </span>
+                        </div>
+                        <div class="h6 position-relative">
+                            <span class="h6 position-relative rounded-pill">AMD001-MIDI JAKARTA BARAT
+                            <span class="small badge rounded-pill bg-success text-light">
+                                <a class="text-decoration-none text-light" title="Jumlah ticket" href="#" data-method="POST">12</a>
+                                <span class="visually-hidden">unread messages</span>
+                            </span>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex flex-row mt-1">
+                    <div class="d-flex flex-column align-items-end ml-2">
+                        <small class="mr-2 text-align-right">Last update 20 days ago</small>
+                        <small class="mr-2 text-align-right">20-08-2023 12:50:24</small>
+                    </div>
+                </div>
+        	</div>
+            <div class="btn-group ms-2 small align-items-end position-absolute top-0 end-0 translate-middle-y">
+            	<button class="btn btn-warning disabled" title="Tidak dicover MC"><i class="fa fa-thumbs-down"></i></button>
+                <button class="btn btn-primary" title="Rekomendasi"><i class="fa fa-handshake"></i></button>
+                <button class="btn btn-primary" title="Teknisi"><i class="fa fa-users-gear"></i></button>
+                <button class="btn btn-primary" title="Pekerjaan"><i class="fa fa-screwdriver-wrench"></i></button>
+                <button class="btn btn-primary" title="Selesai menunggu IT"><i class="fa fa-hourglass"></i></button>
+                <button class="btn btn-success" title="Selesai"><i class="fa fa-circle-check"></i></button>
+                <button class="btn btn-danger" title="Double AHO"><i class="fa fa-bugs"></i></button>
+                <button class="btn btn-primary text-toggle" data-bs-toggle="collapse" href="#ts-ts0001-body" aria-expanded="false" aria-controls="ts-ts0001-body"><i class="text-collapsed fas fa-caret-down"></i><i class="text-expanded fas fa-caret-up"></i></button>
+        	</div>
+    	</div>
+        <div class="card-body collapse" id="ts-ts0001-body">
+        	<div class="text-justify">
+            	<ul class="list-group list-group-flush">
+                	<li class="list-group-item">
+                    	<h6 class="card-title">Rekomendasi</h6>
+                        <p class="card-text">Sebaiknya dilakukan dulu pengecekan area apakah aman untuk pemasangan kamera.</p>
+                    </li>
+                    <li class="list-group-item">
+                    	<h6>Tidak tercover MC</h6>
+                        <p>MC sudah expired sejak 2 hari sebelum pembuatan tiket servis.</p>
+                    </li>
+                    <li class="list-group-item">
+                    	<h6>Daftar Dokumen</h6>
+                        <a href="#">Invoice</a>
+                        <a href="#">SPK</a>
+                    </li>
+                    <li class="list-group-item">
+                    	<h6>Teknisi</h6>
+                        <a href="#">Bambang</a>,
+                        <a href="#">Herianto</a>
+                    </li>
+                    <li class="list-group-item">
+                    	<h6>Pekerjaan</h6>
+                        <div class="row small"><div class="col-2">20-08-2023 12:52.22</div><div class="col-4">Pemeriksaan lapangan</div></div>
+                        <div class="row small"><div class="col-2">21-08-2023 12:52.22</div><div class="col-4">Ganti kamera</div><div class="col-4">Kamera HIKVISION</div><div class="col-2">123456789</div></div>
+                    </li>
+            	</ul>
+        	</div>
+        </div>
+    </div>
