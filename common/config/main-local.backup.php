301e2493559c5b9031d2af9a503037c6f279dbcb
Marsolim <137026909+Marsolim@users.noreply.github.com>
2023-09-30 20:14:47 +0700

    added backup config files

---
+++ common/config/main-local.backup.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+    'components' => [
+        'db' => [
+            'class' => \yii\db\Connection::class,
+            'dsn' => 'mysql:host=localhost;dbname=hardcode_ticketing',
+            'username' => 'hardcode_ticketing',
+            'password' => 'H@rdcode12tiga',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => \yii\symfonymailer\Mailer::class,
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default.
+            //'useFileTransport' => true,
+            'useFileTransport' => false,
+            // You have to set
+            //
+            // 'useFileTransport' => false,
+            //
+            // and configure a transport for the mailer to send real emails.
+            //
+            // SMTP server example:
+            //    'transport' => [
+            //        'scheme' => 'smtps',
+            //        'host' => '',
+            //        'username' => '',
+            //        'password' => '',
+            //        'port' => 465,
+            //        'dsn' => 'native://default',
+            //    ],
+            //
+            // DSN example:
+            //    'transport' => [
+            //        'dsn' => 'smtp://user:pass@smtp.example.com:25',
+            //    ],
+            'transport' => [
+                'dsn' => 'smtp://'.urlencode('admin@ticketing.hardcode.biz.id').':'.urlencode('H@rdcode12tiga').'@ticketing.hardcode.biz.id:465',
+            ],
+            //
+            // See: https://symfony.com/doc/current/mailer.html#using-built-in-transports
+            // Or if you use a 3rd party service, see:
+            // https://symfony.com/doc/current/mailer.html#using-a-3rd-party-transport
+        ],
+    ],
+];
